c8fef98c6b9a51fc77e86526f58485e454c742c3
Santoshkumar <santoshkumar@ss8.com>
2022-10-10 15:09:32 +0530

    header with basic styling

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDliLhN6S4ea7aDY6yVxGIpqVRqge5YttY",
+    authDomain: "challenge-d62a8.firebaseapp.com",
+    projectId: "challenge-d62a8",
+    storageBucket: "challenge-d62a8.appspot.com",
+    messagingSenderId: "147983526691",
+    appId: "1:147983526691:web:b8e5cb10859eea9d417a96",
+    measurementId: "G-XQPRM2SWLF"
+  };
