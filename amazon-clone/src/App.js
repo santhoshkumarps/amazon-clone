98b20e7394c21de8df631049fb2da2bcc1f229de
santhoshkumarps <san14kumarps@gmail.com>
2022-10-17 21:36:51 +0530

    user Login page with registration

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,20 +3,31 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Checkout from './components/product/checkout/Checkout';
+import Login from './components/Login/Login';
 
 function App() {
   return (
 
     <Router>
       <div className="App">
-      <Header />
         <Routes>
+          <Route path='/login'
+                  element={<>
+                    <Login/>
+                  </>}
+                  />
           <Route path='/checkout'
                  element={
                   <>
+                    <Header />
                     <Checkout/>
                   </>}/>
-          <Route path='/' element={<><Home /></>}/>
+          <Route path='/' 
+                 element={
+                    <>
+                      <Header />
+                      <Home />
+                    </>}/>
         </Routes>
       </div>
     </Router>
---
+++ amazon-clone/src/components/Login/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { auth } from '../../firebase-config';
+import './Login.css'
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) =>{
+        e.preventDefault();
+
+        // some fancy firebase login 
+    }
+
+    const registerAccount = (e)=>{
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email,password)
+            .then((auth)=>{
+                //successfully created new user and password
+                console.log(auth);
+            })
+            .catch(error=>console.log(error.message));
+
+        // do some fancy firebase registrations
+    }
+
+
+  return (
+    <div className='login'>
+        <Link to='/'>
+            <img  className='login__logo'
+                  src='https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg'
+                  alt='Amazon logo' />
+        </Link>
+        <div className="login__container">
+            <h1>Sign-in</h1>
+            <form>
+                <h5>E-mail</h5>
+                <input 
+                    type='text' 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    value={email}
+                />
+                <h5>Password</h5>
+                <input 
+                    type='password' 
+                    value={password} 
+                    onChange={(e)=>setPassword(e.target.value)}
+                />
+                <button
+                    type='submit' 
+                    className='login__signInButton'
+                    onClick={signIn}>
+                    Signin
+                </button>
+            </form>
+            <p>
+                By continuing, you agree to Amazon's
+                Conditions of Use and Privacy Notice.
+            </p>
+            <button 
+                className='login__registerButton'
+                onClick={registerAccount}>Create Your Amazon account</button>
+        </div>
+    </div>
+  )
+}
+
+export default Login
